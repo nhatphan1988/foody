2f4515bc12635e62d09d7e1c475f747ed84f3d5c
nhatphan1988 <thamtudethuong@gmail.com>
2016-01-03 21:32:39 +0800

    [Add]

---
+++ FoodyDomain/App_Code/CustomAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyDomain.App_Code
+{
+    [AttributeUsage(AttributeTargets.Property |
+    AttributeTargets.Field, AllowMultiple = false)]
+    sealed public class CustomAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return false;
+        }
+        
+        public override string FormatErrorMessage(string name)
+        {
+            return "Custom Validation Error";
+        }
+    }
+}
